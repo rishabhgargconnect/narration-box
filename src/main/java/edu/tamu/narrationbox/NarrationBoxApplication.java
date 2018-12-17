6cb053caab6cae9040d5b546c2ab6beb126750c6
Nikhil Bhat <nbhat510@gmail.com>
2018-12-17 15:03:06 +0530

    Added sample controller and functionality for automatically generating swagger documentation from api documentation
    Signed-off-by: Nikhil Bhat <nbhat510@gmail.com>

--- src/main/java/edu/tamu/narrationbox/NarrationBoxApplication.java
+++ src/main/java/edu/tamu/narrationbox/NarrationBoxApplication.java
@@ -2,19 +2,14 @@ package edu.tamu.narrationbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @RestController
 public class NarrationBoxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NarrationBoxApplication.class, args);
-    }
-
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "Here is your story, now deployed on HEROKU....";
     }
 }
