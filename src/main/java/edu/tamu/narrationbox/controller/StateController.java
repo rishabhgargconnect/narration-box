6c21451343d71c6061f3f56eaae215147c4df8b2
Nikhil Bhat <nbhat510@gmail.com>
2019-01-08 22:55:12 +0530

    Added CRUD operations on Controllers
    Signed-off-by: Nikhil Bhat <nbhat510@gmail.com>

--- src/main/java/edu/tamu/narrationbox/controller/StateController.java
+++ src/main/java/edu/tamu/narrationbox/controller/StateController.java
@@ -1,22 +1,47 @@
 package edu.tamu.narrationbox.controller;
 
-import edu.tamu.narrationbox.model.States.State;
+import edu.tamu.narrationbox.model.State;
+import edu.tamu.narrationbox.repository.StateRepository;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/states/")
 @Api(description = "The possible states of the characters which can be of types: emotional, physical, existential etc.")
 public class StateController{
 
+    @Autowired
+    public StateRepository stateRepository;
+
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get all the states registered in the system.")
     public State[] getAllStates(){
-        return new State[10];
+        return stateRepository.findAll().toArray(new State[0]);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation("Register a state in the system.")
+    public String createStates(@RequestBody State state) {
+        stateRepository.save(state);
+        return "Success";
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    @ApiOperation("Update a registered state in the system.")
+    public String updateState(@PathVariable("id") String id, @RequestBody State state) {
+        if(!state.getId().equals(id))
+            return "Error";
+
+        stateRepository.save(state);
+        return "Success";
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ApiOperation("Delete a state registered in the system.")
+    public String deleteState(@PathVariable("id") String id) {
+        stateRepository.deleteById(id);
+        return "Success";//TODO: Improve return mechanism
     }
 }
