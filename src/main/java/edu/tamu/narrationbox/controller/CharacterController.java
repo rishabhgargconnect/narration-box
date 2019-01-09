e72c92e5a86aeaa731f2ccf4c210610803a0abdb
Nikhil Bhat <nbhat510@gmail.com>
2019-01-10 00:04:59 +0530

    Added route for get resource by Id
    Signed-off-by: Nikhil Bhat <nbhat510@gmail.com>

--- src/main/java/edu/tamu/narrationbox/controller/CharacterController.java
+++ src/main/java/edu/tamu/narrationbox/controller/CharacterController.java
@@ -1,36 +1,53 @@
 package edu.tamu.narrationbox.controller;
 
 import edu.tamu.narrationbox.model.Character;
+import edu.tamu.narrationbox.repository.CharacterRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/characters/")
-@Api(description = "The possible characters of the characters which can be of types: emotional, physical, existential etc.")
-public class CharacterController {
+@Api(description = "The descriptions of the characters in the story")
+public class CharacterController{
+
+    @Autowired
+    public CharacterRepository characterRepository;
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get all the characters registered in the system.")
     public Character[] getAllCharacters(){
-        return new Character[10];
+        return characterRepository.findAll().toArray(new Character[0]);
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation("Get a character registered in the system.")
+    public Character getCharacter(@PathVariable("id") String id){
+        return characterRepository.findById(id).orElse(null);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ApiOperation("Register a character in the system.")
     public String createCharacters(@RequestBody Character character) {
-        return "";
+        characterRepository.save(character);
+        return "Success";
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     @ApiOperation("Update a registered character in the system.")
     public String updateCharacter(@PathVariable("id") String id, @RequestBody Character character) {
-        return "";
+        if(!character.getId().equals(id))
+            return "Error";
+
+        characterRepository.save(character);
+        return "Success";
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     @ApiOperation("Delete a character registered in the system.")
     public String deleteCharacter(@PathVariable("id") String id) {
-        return "";
+        characterRepository.deleteById(id);
+        return "Success";//TODO: Improve return mechanism
     }
 }
