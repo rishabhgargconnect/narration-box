c3a75415cc81c88154b99fb6d6f09b39787a6b7a
Nikhil Bhat <nbhat510@gmail.com>
2019-04-18 16:45:54 -0500

    new: script to upload images

--- src/scripts/script_image_upload.py
+++ src/scripts/script_image_upload.py
@@ -1,11 +1,16 @@
+#%%
 import json
 import base64
 import requests
 import os
 
+#%%
 URL = "https://narration-box.herokuapp.com/images/"
 
-selected_option = int(input("Select your option:\n1. Upload a image. \n2. Upload a folder of images."))
+def parse_for_characters(directory_path): 
+    for root, _, files in os.walk(directory_path):
+        if files is not None and 'default.png'  in files or 'default.psd' in files:
+            yield root
 
 def base64_encode_file_contents(path_of_file):
     try:
@@ -16,39 +21,69 @@ def base64_encode_file_contents(path_of_file):
         print("No such file as "+ path_of_file)
         return ""
 
+
+def upload_folder(path_of_folder, root):
+    name_of_folder = os.path.basename(path_of_folder)
+    if(str.isdigit(name_of_folder)):
+        print(f"Character name cannot be {path_of_folder}. Please select a better name.")
+        return
+    for file in os.listdir(path_of_folder):
+        full_file_path = os.path.join(path_of_folder, file)
+        if (file in [".svn","Thumbs.db"]):
+            continue
+        if os.path.isdir(full_file_path):
+            continue
+        
+        encoded_string = base64_encode_file_contents(full_file_path)
+        json_data = {
+            'path': str.lower(str.replace(os.path.relpath(path_of_folder, root), '\\', '/')),
+            'identity': str.lower(name_of_folder),
+            'emotion' : str.lower(os.path.splitext(file)[0]),
+            'file' : str(encoded_string)
+        }
+        print(json_data)
+        headers = {'content-type':'application/json'}
+        r = requests.post(URL, data = json.dumps(json_data), headers = headers)
+
+#%%
+selected_option = int(input("Select your option:\n1. Upload a image. \n2. Upload a folder of images.\n3. Upload a directory containing multiple folders"))
+root_folder = input('Enter root folder:\n')
+
+#%%
 if(selected_option == 1):
+    print('Feature down. Go catch the developer')
+    """
     path_of_file = input('Enter path of file:')
     identity = input('What is this character?').lower()
     emotion =  input('How does this character feel right now?\n').lower()
     encoded_string = base64_encode_file_contents(path_of_file)
     json_data = {
-        'identity': identity,
-        'emotion': emotion,
-        'file' : str(encoded_string, 'utf-8')
+        'path': str.replace(os.path.relpath(path_of_file, root_folder), '\\', '/'),
+        'identity': name_of_folder,
+        'emotion' : os.path.splitext(file)[0],
+        'file' : str(encoded_string)
     }
     print(json_data)
     headers = {'content-type':'application/json'}
     r = requests.post(URL, data = json.dumps(json_data), headers = headers)
-    print(r)
-
+    """
 elif selected_option == 2:
     path_of_folder = input('Enter path of folder:')
     name_of_folder = os.path.basename(path_of_folder)
     print("**************Begining to upload. Folder name is considered as the type of the character" +
         "and the name of the emotion is considered to be its emotion.*******************")
-    for file in os.listdir(path_of_folder):
-        full_file_path = os.path.join(path_of_folder, file)
-        encoded_string = base64_encode_file_contents(full_file_path)
-        json_data = {
-            'identity': name_of_folder,
-            'emotion' : os.path.splitext(file)[0],
-            'file' : str(encoded_string)
-        }
-        #print(json_data)
-        headers = {'content-type':'application/json'}
-        r = requests.post(URL, data = json.dumps(json_data), headers = headers)
-        print(r)
+    upload_folder(name_of_folder, root_folder)
+
+    print("***********Upload finished*************")
+
+elif selected_option == 3:
+    for path_of_character in parse_for_characters(root_folder):
+        upload_folder(path_of_character, root_folder)
+    print("**************Begining to upload. Folder name is considered as the type of the character " +
+        "and the name of the emotion is considered to be its emotion.*******************")
 
     print("***********Upload finished*************")
 else:
-    print("Option menu: I am a joke to you?")
+    print("Please enter valid options")
+
+#%%
