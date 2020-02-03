0c735c12017c0b6198bba941fc20eb95596772b1
Rishabh Garg <rgarg@client-10-229-224-237.tamulink.tamu.edu>
2020-02-03 15:53:23 -0600

    test

--- src/main/java/edu/tamu/narrationbox/controller/ImageController.java
+++ src/main/java/edu/tamu/narrationbox/controller/ImageController.java
@@ -23,13 +23,13 @@ public class ImageController {
 
     @RequestMapping(value = "default", method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get the default pic of all the images registered in the system.")
-    public Image[] getDefaultImages(){
+    public Image[] getDefaultImages() {
         return imageRepository.findDefaultImageOfAllCharacters().toArray(new Image[0]);
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get all the images registered in the system.")
-    public Image[] getAllImages(){
+    public Image[] getAllImages() {
         return imageRepository.findAll().toArray(new Image[0]);
     }
 
@@ -38,14 +38,14 @@ public class ImageController {
             produces = MediaType.IMAGE_PNG_VALUE)
     @ApiOperation("Get a image registered in the system.")
     public ResponseEntity<byte[]> getImage(@PathVariable("identityId") String identityId,
-                    @RequestParam(value = "emotion", required = false) String emotion){
+                                           @RequestParam(value = "emotion", required = false) String emotion) {
         List<Image> images = imageRepository.findByImageMatchingAttributes(identityId, emotion);
-        if(images.isEmpty()){
+        if (images.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         String image = images.get(0).getFile();
         return new ResponseEntity<>(Base64.getDecoder().decode
-                (image.substring(2, image.length()-1)), HttpStatus.OK); //Remove /b which gets added from the python script
+                (image.substring(2, image.length() - 1)), HttpStatus.OK); //Remove /b which gets added from the python script
 
     }
 
@@ -54,11 +54,10 @@ public class ImageController {
             produces = MediaType.IMAGE_PNG_VALUE)
     @ApiOperation("Delete a specific emotion of an image or entire image from the system.")
     public String deleteImage(@PathVariable("identityId") String identityId,
-                           @RequestParam(value = "emotion", required = false) String emotion){
-        if(emotion != null && emotion != ""){
+                              @RequestParam(value = "emotion", required = false) String emotion) {
+        if (emotion != null && emotion != "") {
             imageRepository.deleteImageMatchingAttributes(identityId, emotion);
-        }
-        else {
+        } else {
             imageRepository.deleteImageIdentity(identityId);
         }
         return "Success";
@@ -66,25 +65,34 @@ public class ImageController {
 
     @RequestMapping(value = "ids", method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get the ids of images registered in the system.")
-    public String[] getAllImageIds(){
-            return imageRepository.findAll().stream().map(  x->x.getIdentity()).distinct().toArray(String[]::new);
+    public String[] getAllImageIds() {
+        return imageRepository.findAll().stream().map(x -> x.getIdentity()).distinct().toArray(String[]::new);
     }
 
     @RequestMapping(value = "node", method = RequestMethod.GET, produces = "application/json")
     @ApiOperation("Get the node in the system.")
-    public TreeNode getNode(@RequestParam(value = "category", required = false) String path){
-        if(path == null){
+    public TreeNode getNode(@RequestParam(value = "category", required = false) String path) {
+        System.out.println("Hello");
+
+        if (path == null) {
             path = "";
+            String[] a = {"as", "qw"};
+            TreeNode node = new TreeNode();
+            node.setCategories(a);
+            return node;
         }
 
-        String regexPath = MessageFormat.format("^{0}\\/[\\w-]+(?!\\/)$", path) ;
+        String regexPath = MessageFormat.format("^{0}\\/[\\w-]+(?!\\/)$", path);
+        System.out.println("regexPath = " + regexPath);
         TreeNode node = new TreeNode();
-        node.setCategories(imageRepository.getImagesOnPath(regexPath, "category").stream().map(x-> (x.getPath())).toArray(String[]::new));
-        node.setCharacters(imageRepository.getImagesOnPath(regexPath, "character").stream().map(x-> getNameOfCharacter(x.getPath())).toArray(String[]::new));
+        node.setCategories(imageRepository.getImagesOnPath(regexPath, "category").stream().map(x -> (x.getPath())).toArray(String[]::new));
+        node.setCharacters(imageRepository.getImagesOnPath(regexPath, "character").stream().map(x -> getNameOfCharacter(x.getPath())).toArray(String[]::new));
+        String[] a = {"as", "qw"};
+        node.setCategories(a);
         return node;
     }
 
-    private String getNameOfCharacter(String s){
+    private String getNameOfCharacter(String s) {
         return s.substring(s.lastIndexOf('/') + 1).trim();
     }
 
